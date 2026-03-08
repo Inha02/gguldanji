e77d37fca323378e2cde81b1d5eef89fa0afbf11
최인하 <bella020207@naver.com>
2026-03-08 21:04:51 +0900

    chore: socketIO init

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import cors from "cors";
+import http from "http";
+
 import router from "./routes/user.routes.js";
 import authRouter from "./routes/auth.js";
 import postRoutes from "./routes/post.routes.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./config/swagger.js";
 import chatRoutes from "./routes/chat.routes.js";
+
+
+import  { Server } from "socket.io";
+import { initSocket }  from "./socket/chat.socket.js";
+
 import "./models/User.js";
+import "./models/ChatRoom.js";
+import "./models/ChatMessage.js";
+
 
 dotenv.config();
 
@@ -24,6 +34,18 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.get("/", (req, res) => {
   res.send("Backend server running!");
 });
+
+// HTTP 서버 생성
+const server = http.createServer(app);
+
+// Socket.IO 초기화
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+initSocket(io);
 
 // API 라우터 
 app.use("/users", router);
---
+++ backend/socket/chat.socket.js
@@ -0,0 +1,115 @@
+import ChatMessage from "../models/ChatMessage.js";
+import ChatRoom from "../models/ChatRoom.js";
+
+/**
+ * Socket 초기화
+ */
+export const initSocket = (io) => {
+
+  io.on("connection", (socket) => {
+
+    console.log("user connected:", socket.id);
+
+    /**
+     * 채팅방 입장
+     */
+    socket.on("join_room", (roomId) => {
+
+      socket.join(roomId);
+
+      console.log(`socket ${socket.id} joined room ${roomId}`);
+    });
+
+
+    /**
+     * 메시지 전송
+     */
+    socket.on("send_message", async (data) => {
+
+      try {
+
+        const { roomId, senderId, content, image } = data;
+
+        /**
+         * MongoDB 저장
+         */
+        const newMessage = await ChatMessage.create({
+          roomId,
+          senderId,
+          content: content || "",
+          image: image || "",
+          aiSuggestion: [],
+          isBadManner: false,
+
+          // 보낸 사람은 읽음
+          readBy: [senderId]
+        });
+
+        /**
+         * 채팅방 updatedAt 업데이트
+         */
+        await ChatRoom.findByIdAndUpdate(
+          roomId,
+          { updatedAt: new Date() }
+        );
+
+        /**
+         * DB 저장된 메시지 조회
+         */
+        const populatedMessage = await ChatMessage.findById(newMessage._id)
+          .populate("senderId", "nickname profileImage");
+
+        /**
+         * 같은 room 사용자에게 전송
+         */
+        io.to(roomId).emit("receive_message", populatedMessage);
+
+      } catch (error) {
+
+        console.error("message error:", error);
+
+      }
+
+    });
+
+
+    /**
+     * 연결 종료
+     */
+    socket.on("disconnect", () => {
+
+      console.log("user disconnected:", socket.id);
+
+    });
+
+  });
+
+};
+
+/*
+ 프론트 채팅 메시지 예시
+ {
+  "roomId": "65c1b6c2e3f4a123456789ab",
+  "senderId": "65c1b6c2e3f4a123456789bb",
+  "content": "가격 네고 가능할까요?",
+  "image": ""
+}
+
+프론트 소켓 예시
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:4000");
+
+socket.emit("join_room", roomId);
+
+socket.emit("send_message", {
+  roomId,
+  senderId,
+  content: message
+});
+
+socket.on("receive_message", (data) => {
+  console.log(data);
+});
+
+ */
